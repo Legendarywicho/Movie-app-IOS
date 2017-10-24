d15f69cd84f72f5f09a92ad3cd725492eb84c07d
Luis Fernando <xvleg3ndaryxv@gmail.com>
2017-10-23 20:53:57 -0500

    Initial Commit

---
+++ GuessMovie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuessMovie
+//
+//  Created by Luis Santiago  on 10/23/17.
+//  Copyright © 2017 Luis Santiago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
